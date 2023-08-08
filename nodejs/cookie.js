400a7538ea98a1735888af9531ab8b5754cb50a9
Dexter <mystyle1057@gmail.com>
2023-08-08 12:24:43 +0900

    html cookie class finished

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -10,7 +10,15 @@ http.createServer(function(request, response) {
     console.log(cookies.yummy_cookie);
 
     response.writeHead(200, {
-        'Set-Cookie':['yummy_cookie=choco', 'tasty_cookie=strawberry']
+        'Set-Cookie':[
+            'yummy_cookie=choco',
+            'tasty_cookie=strawberry',
+            `Peranent=cookies; Max-Age=${60*60*24*30}`,
+            'Secure=Secure; Secure',                    // Https로 접근 제한
+            'HttpOnly=HttpOnly; HttpOnly',              // JavaScript로 접근 제한
+            'Path=Path; Path=/cookie',                  // 경로 제한
+            'Domain=Domain; Domain=o2.org'              // 도메인 제한
+        ]
     });
     response.end('Cookie!!');
 }).listen(3000);
